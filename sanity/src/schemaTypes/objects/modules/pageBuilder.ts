40d71328233ba307209489ce0b165d0557281674
sonsofnero <matt@forefathersgroup.com>
2026-09-17 11:11:47 -0400

    fix(studio): repair page-builder insert menu previews

--- sanity/src/schemaTypes/objects/modules/pageBuilder.ts
+++ sanity/src/schemaTypes/objects/modules/pageBuilder.ts
@@ -1,5 +1,8 @@
 import {defineArrayMember, defineType} from 'sanity'
 
+/** Slice types with a screenshot at sanity/static/<type>.png. */
+const SLICE_PREVIEWS = new Set(['exampleSlice'])
+
 export const pageBuilderType = defineType({
   name: 'pageBuilder',
   title: 'Page Builder',
@@ -10,7 +13,16 @@ export const pageBuilderType = defineType({
   ],
   options: {
     insertMenu: {
-      views: [{name: 'grid', previewImageUrl: (schemaTypeName) => `/static/${schemaTypeName}.png`}],
+      views: [
+        {
+          name: 'grid',
+          previewImageUrl: (schemaTypeName) =>
+            SLICE_PREVIEWS.has(schemaTypeName)
+              ? `/static/${schemaTypeName}.png`
+              : undefined,
+        },
+        {name: 'list'},
+      ],
     },
   },
 })
